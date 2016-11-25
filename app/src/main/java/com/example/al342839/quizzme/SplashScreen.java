c3a78594f1b69bdd83f81166c47f22e84bfd630a
gaby492 <gabriela.dominguez@uabc.edu.mx>
2016-11-24 16:30:52 -0800

    Splash

---
+++ app/src/main/java/com/example/al342839/quizzme/SplashScreen.java
@@ -0,0 +1,52 @@
+package com.example.al342839.quizzme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+/**
+ * Created by al341732 on 11/24/2016.
+ */
+
+public class SplashScreen extends Activity {
+    private ProgressBar mProgress;
+    ImageView img;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Show the splash screen
+        setContentView(R.layout.splash_screen);
+        mProgress = (ProgressBar) findViewById(R.id.splash_screen_progress_bar);
+        img= (ImageView) findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.logo);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                startApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void doWork() {
+        for (int progress=0; progress<100; progress+=10) {
+            try {
+                Thread.sleep(1000);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+                //Timber.e(e.getMessage());
+            }
+        }
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
