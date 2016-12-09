6a9411b7c9254c4ca197d4496f8674a8d78676e7
Anairene Ishihara <anairene.ishihara@uabc.edu.mx>
2016-12-08 17:34:50 -0800

    Se integró Activity About, y cambios en la vista.

---
+++ app/src/main/java/com/example/al342839/quizzme/Acerca.java
@@ -0,0 +1,29 @@
+package com.example.al342839.quizzme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Acerca extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView acercaTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca);
+        usarToolbar();
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        acercaTextView = (TextView) findViewById(R.id.acercaTextView);
+        imageView.setImageResource(R.drawable.creadoras);
+
+    }
+
+    private void usarToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
--- app/src/main/java/com/example/al342839/quizzme/MainActivity.java
+++ app/src/main/java/com/example/al342839/quizzme/MainActivity.java
@@ -133,7 +133,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.acerca) {
+            Intent intent = new Intent(this, Acerca.class);
+            this.startActivity(intent);
             return true;
         }
 
